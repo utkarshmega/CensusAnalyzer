f9a86f9904db80690ab12f54dc0cfbe8413eed79
utkarshmega <utkarsheduc@gmail.com>
2020-10-19 23:59:49 +0530

    Welcome to state analyser problem

---
+++ src/main/java/com/capgemini/CensusAnalyzer/StateCensusAnayzer.java
@@ -0,0 +1,10 @@
+package com.capgemini.CensusAnalyzer;
+
+public class StateCensusAnayzer {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to indian state census analyzer problem");
+	}
+
+}
